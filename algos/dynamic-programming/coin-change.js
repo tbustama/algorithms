1a2ba5ad0a7a0a22c70f61b3753e909200d70087
Taylor Bustamante <taylorbustamante16@gmail.com>
2022-06-22 12:11:41 -0500

    editing coin change

--- algos/dynamic-programming/coin-change.js
+++ algos/dynamic-programming/coin-change.js
@@ -2,7 +2,7 @@ var coinChange = function (coins, amount) {
   let res = new Array(amount + 1).fill(Infinity);
   res[0] = 0;
   for (let i = 1; i < amount + 1; i++) {
-    for (coin of coins) {
+    for (let coin of coins) {
       if (i - coin >= 0) {
         res[i] = Math.min(res[i], 1 + res[i - coin]);
       }
