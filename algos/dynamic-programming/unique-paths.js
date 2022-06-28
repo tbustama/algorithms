2e2b7d1be41ef6b7c2786f3b25a5b9dd2eb40b89
Taylor Bustamante <taylorbustamante16@gmail.com>
2022-06-28 18:12:23 -0500

    unique paths

---
+++ algos/dynamic-programming/unique-paths.js
@@ -0,0 +1,12 @@
+var uniquePaths = function (m, n) {
+  let row = new Array(n).fill(1);
+  for (let i = m - 2; i >= 0; i--) {
+    let newRow = new Array(n).fill(1);
+    for (let j = n - 2; j >= 0; j--) {
+      newRow[j] = row[j] + newRow[j + 1];
+    }
+    row = newRow;
+  }
+
+  return row[0];
+};
