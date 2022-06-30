1315e570d02cd7ab8919ec43c77771a8c5b2f449
Taylor Bustamante <taylorbustamante16@gmail.com>
2022-06-29 21:55:00 -0500

    optimized

--- algos/strings/longest-repeating-char.js
+++ algos/strings/longest-repeating-char.js
@@ -2,13 +2,15 @@ var characterReplacement = function (s, k) {
   let map = new Map();
   let res = 0;
   let l = 0;
+  let maxF = 0;
   for (let r in s) {
     if (map.get(s[r])) {
       map.set(s[r], map.get(s[r]) + 1);
     } else {
       map.set(s[r], 1);
     }
-    while (r - l + 1 - Math.max(...map.values()) > k) {
+    maxF = Math.max(maxF, map.get(s[r]));
+    while (r - l + 1 - maxF > k) {
       map.set(s[l], map.get(s[l]) - 1);
       l += 1;
     }
