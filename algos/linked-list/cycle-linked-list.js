ed65e6fe46bfc06a812cf8698fae8939b250a73d
Taylor Bustamante <taylorbustamante16@gmail.com>
2022-07-14 21:08:18 -0500

    cycle linked list

---
+++ algos/linked-list/cycle-linked-list.js
@@ -0,0 +1,12 @@
+var hasCycle = function (head) {
+  let set = new Set();
+  while (head) {
+    if (set.has(head)) {
+      return true;
+    } else {
+      set.add(head);
+      head = head.next;
+    }
+  }
+  return false;
+};
