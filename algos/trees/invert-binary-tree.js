231ba8d495dd15110a76b8cb3709c2a08c00aaa0
Taylor Bustamante <taylorbustamante16@gmail.com>
2022-07-17 11:16:58 -0500

    invert binary tree

---
+++ algos/trees/invert-binary-tree.js
@@ -0,0 +1,12 @@
+var invertTree = (root) => {
+  if (!root) {
+    return null;
+  }
+  let temp = root.left;
+  root.left = root.right;
+  root.right = temp;
+  invertTree(root.left);
+  invertTree(root.right);
+
+  return root;
+};
--- algos/trees/max-depth-binary.js
+++ algos/trees/max-depth-binary.js
@@ -1,15 +1,15 @@
 var maxDepth = function (root) {
-  if (!root) {
-    return 0;
-  }
-  let max = 1;
-  let current = 1;
+  //   if (!root) {
+  //     return 0;
+  //   }
+  let max = 0;
+  let current = 0;
   let dfs = function (node, current) {
     if (!node) {
       return;
     }
-    max = Math.max(current, max);
     current += 1;
+    max = Math.max(current, max);
     dfs(node.left, current);
     dfs(node.right, current);
   };
