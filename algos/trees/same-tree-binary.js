a05c2aa10ffc979cd8a7d1de322fd42f407a5e18
Taylor Bustamante <taylorbustamante16@gmail.com>
2022-07-19 21:35:08 -0500

    refactoring same tree

--- algos/trees/same-tree-binary.js
+++ algos/trees/same-tree-binary.js
@@ -1,23 +1,10 @@
 var isSameTree = function (p, q) {
-  let result = true;
-  const simDFS = (node1, node2) => {
-    if (node1 === null && node2 === null) {
-      return;
-    }
-    if (
-      (node1 === null && node2 !== null) ||
-      (node1 !== null && node2 === null)
-    ) {
-      result = false;
-      return;
-    }
-    if (node1.val !== node2.val) {
-      result = false;
-      return;
-    }
-    simDFS(node1.left, node2.left);
-    simDFS(node1.right, node2.right);
-  };
-  simDFS(p, q);
-  return result;
+  if (!p && !q) {
+    return true;
+  }
+
+  if (p && q && p.val === q.val) {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+  return false;
 };
